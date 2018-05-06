1ceb7f7a861b7149402e4f8666c7a4e2a007c4ed
Kev Kipkemoi <kevkipkemoi@outlook.com>
2018-05-06 14:15:09 +0300

    axis live data

--- src/components/Tickers.js
+++ src/components/Tickers.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Tickers.css';
-import Cryptocurrency from './Cryptocurrency';
+import CryptoCurrency from './CryptoCurrency';
+import axios from 'axios';
 
 class Tickers extends Component {
 
@@ -39,16 +40,33 @@ class Tickers extends Component {
         };
     }
 
+    // when the component did load, the data is loaded and updated every 10 seconds
+    componentDidMount () {
+        this.fetchCryptocurrencyData();
+        this.interval = setInterval(() => this.fetchCryptocurrencyData(), 10 * 1000);
+    }
+
+    // Get data from coinmaerket API with Axios
+    fetchCryptocurrencyData() {
+        axios.get("https://api.cinmarketcap.com/v1/ticker/?limit=5")
+            .then(response => {
+                var wanted = ["bitcoin", "ethereum", "litecoin"];
+                var result = response.data.filter(currency => wanted.includes(currency.id));
+                this.setState({data: result});
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
-        var tickers = this.state.data.map((currency) =>
-            <Cryptocurrency data={currency} key={currency.id} />
-    );
+        let tickers = this.state.data.map((currency) => 
+            <CryptoCurrency data={currency} key={currency.id} />
+        );
+    
     return (
         
          <div className="tickers-container">
             <ul className="tickers">{tickers}</ul>
-            <p>Information updated every 10 seconds courtesy of coinmarketcap.com</p>
-        </div>
+         </div>
         
     );
     }
